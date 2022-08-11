330e6dffdf9e05e6ea22f551e102df20d398dc4c
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-11 16:57:45 +0300

    feat: use Type-Only Imports and Exports

--- TS-In-Depth-master/src/app.ts
+++ TS-In-Depth-master/src/app.ts
@@ -1,4 +1,4 @@
-import { ReferenceItem, RefBook, UniversityLibrarian } from './classes';
+import { ReferenceItem, RefBook, UniversityLibrarian, Library } from './classes';
 import { getAllBooks } from './functions';
 
 showHello('greeting', 'TS');
@@ -9,12 +9,20 @@ function showHello(divName: string, name: string) {
 }
 
 // Task 06.05
-const flag = true;
-if (flag) {
-    import('./classes').then(module => {
-        const reader = new module.Reader();
-        reader.name = 'Anna';
-        reader.take(getAllBooks()[0]);
-        console.log(reader);
-    });
-}
+// const flag = true;
+// if (flag) {
+//     import('./classes').then(module => {
+//         const reader = new module.Reader();
+//         reader.name = 'Anna';
+//         reader.take(getAllBooks()[0]);
+//         console.log(reader);
+//     });
+// }
+
+// Task 06.06
+// const library = new Library();
+let lib: Library = {
+    id: 1,
+    name: 'Anna',
+    address: 'Vilnius',
+};
--- TS-In-Depth-master/src/classes/index.ts
+++ TS-In-Depth-master/src/classes/index.ts
@@ -2,3 +2,4 @@ export * from './reference-item';
 export * from './university-librarian';
 export * from './reader';
 export { default as RefBook } from './encyclopedia';
+export type { Library } from './library';
